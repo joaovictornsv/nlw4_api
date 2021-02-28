09ce1a6c997c303f0489eb3e6de0651acf2d36da
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-02-28 14:14:14 -0300

    app ok

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
 import router from './routes'
-import './database'
+import createConnection from './database'
 import MiddlewareException from '../src/errors/MiddlewareException'
 
+createConnection()
 const app = express()
 
 app.use(express.json())
