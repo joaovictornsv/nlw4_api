28f4616214bc8d85c9e503fcbbce1437e64eab24
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-03-03 09:56:02 -0300

    create nps calculator service

---
+++ src/services/NpsCalcService.ts
@@ -0,0 +1,39 @@
+import { SurveysUsersRepository } from '@repositories/SurveysUsersRepository'
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+
+class NpsCalcService {
+  async execute (request: Request, response: Response) {
+    const { survey_id } = request.params
+
+    const surveysUserRepository = getCustomRepository(SurveysUsersRepository)
+
+    const surveysUsers = await surveysUserRepository.find({ survey_id })
+
+    const promoters = surveysUsers.filter(
+      (survey) => survey.value === 9 || survey.value === 10
+    ).length
+
+    const detractors = surveysUsers.filter(
+      (survey) => survey.value >= 0 || survey.value <= 6
+    ).length
+
+    const passives = surveysUsers.filter(
+      (survey) => survey.value === 7 || survey.value === 8
+    ).length
+
+    const totalAnswers = surveysUsers.length
+
+    const score = (promoters - detractors) / totalAnswers * 100
+
+    return {
+      score,
+      totalAnswers,
+      promoters,
+      passives,
+      detractors
+    }
+  }
+}
+
+export { NpsCalcService }
